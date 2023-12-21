3a4073ae3612c49c037721547ee19e5986b72198
IsraelVidalParedes <A01750543@tec.mx>
2023-12-21 02:02:16 -0600

    feat(assets): added poppins font family to files

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      theme: ThemeData(fontFamily: 'Poppins'),
+      home: const HomePage(),
     );
   }
 }
